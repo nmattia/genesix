23526c6983924748b9ce58c9a861ece8fe398b59
Nicolas Mattia <nicolas@nmattia.com>
2018-11-06 10:06:59 +0400

    Simplify HTML library

--- genesix-html/html.nix
+++ genesix-html/html.nix
@@ -1,68 +1,68 @@
 # The HTML library
 {pkgs, genesix-lib, root, file, pathOf, relpath}:
 rec {
-  saturatePretag = tag:
-    let
-      res =
-        if builtins.isFunction tag then
-          if builtins.isFunction (tag null)
-          then tag null null
-          else tag null
-        else tag;
-    in
-      if builtins.isAttrs res then res else
-      abort "Saturation failed, leftover type is ${builtins.typeOf res}";
-
-  mkPretag = tagName:
-    arg1: arg2: { inherit arg1 arg2 tagName; };
-
-  # TODO: if tag content or attr value has context, abort
-  pretag2Tag = pretag:
-    let
-        pretag' = saturatePretag pretag;
-        attrs = if builtins.isNull pretag'.arg1 then {} else pretag'.arg1;
-        tagname = pretag'.tagname;
-        renderedAttrs =
-          pkgs.lib.concatStrings (
-            pkgs.lib.attrsets.mapAttrsToList
-            # TODO: do the relpath thing IFF v is a path
-            (k: v: " ${k}=\"${relpath v}\"")
-            attrs
-            );
-    in if builtins.isNull pretag'.arg2 then
-        "<" + pretag'.tagName + renderedAttrs + "/>" else
-        "<" + pretag'.tagName + renderedAttrs + ">" +
-          pretag'.arg2 + "</" + pretag'.tagName + ">";
 
   # All HTML tags
   tags =
     pkgs.lib.foldl
-      (xs: x: xs // { "${x}" = mkPretag x; })
+      (xs: x: xs // { "${x}" = mkHTMLTag x; })
       {}
       (pkgs.lib.strings.splitString "\n" (pkgs.lib.readFile ./html-tags));
 
-  # All these should return a list of pretags
-  renderPretag = pretag: [(pretag2Tag pretag)];
+  # Rendering { foo = "bar"; } to foo="bar"
+  renderHTMLAttributes = attrs:
+          pkgs.lib.concatStrings (
+            pkgs.lib.attrsets.mapAttrsToList
+            # TODO: do the relpath thing IFF v is a path
+            (k: v:
+              let
+                v' =
+                  if builtins.typeOf v == "path"
+                  then relpath v
+                  else builtins.toString v;
+              in " ${k}=\"${v'}\"")
+            attrs
+            );
 
-  renderListOfPretags = map pretag2Tag;
+  # Feed with nulls until it's not a function anymore
+  saturate = pkgs.lib.fix (f: v:
+    if builtins.isFunction v then f (v null) else v);
+
+  mkHTMLTag = tagName:
+    arg1: arg2:
+      let pretag =
+        { inherit arg1 arg2 tagName; };
+      in
+    let
+        attrs = if builtins.isNull pretag.arg1 then {} else pretag.arg1;
+        tagname = pretag.tagname;
+        renderedAttrs = renderHTMLAttributes attrs;
+    in if builtins.isNull pretag.arg2 then
+        "<" + pretag.tagName + renderedAttrs + "/>" else
+        "<" + pretag.tagName + renderedAttrs + ">" +
+          pretag.arg2 + "</" + pretag.tagName + ">";
+
+  xmlTag = attrs:
+    let
+      renderedAttrs = renderHTMLAttributes attrs;
+    in
+      "<?xml${renderedAttrs}?>";
 
   srcpath =
     genesix-lib.mkSrcPath { inherit root; from = file;};
 
   render = input:
     let
-      listOfPretags =
-        if builtins.isList input then renderListOfPretags input else
-        if builtins.isAttrs input then renderPretag input else
-        abort "Cannot deal with builtins.typeOf input";
-    in render' listOfPretags;
-
-  render' = tags:
-    { content = pkgs.lib.concatStrings tags;
-      outpath =
-        let
-          last = pkgs.lib.last srcpath;
-          last' = pkgs.lib.removeSuffix ".nix" last;
-        in  pkgs.lib.init srcpath ++ [ last' ];
-    };
+      str =
+        if builtins.isList input
+        then pkgs.lib.concatMapStrings saturate input
+        else saturate input;
+    in
+      { content = str;
+        outpath =
+          let
+            last = pkgs.lib.last srcpath;
+            last' = pkgs.lib.removeSuffix ".nix" last;
+          in  pkgs.lib.init srcpath ++ [ last' ];
+      };
 }
--- tests/08_html_links/files/index.html.nix
+++ tests/08_html_links/files/index.html.nix
@@ -1,2 +1,4 @@
 { html }:
-[ (html.tags.a { href = ./page.html.nix; } "Go to some other page?") ]
+[ (html.xmlTag { version = "1.0"; encoding = "UTF-8"; })
+  (html.tags.a { href = ./page.html.nix; } "Go to some other page?")
+]
